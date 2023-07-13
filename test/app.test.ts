b34aef3d5a79b311639098dcd04c3f771b5a9a0f
luisangeluis <luisangeluis@gmail.com>
2023-07-13 15:30:41 -0500

    Making test to end points

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,7 +3,23 @@ import request from "supertest";
 
 describe("GET /products", () => {
   test("Should respond with 200 status code", async () => {
-    const response = await request(app).get("/products").send();
+    const response = await request(app).get("/api/v1/products").send();
     expect(response.statusCode).toBe(200);
   });
+
+  test("Should respond with an array",async()=>{
+    const response = await request(app).get("/api/v1/products").send();
+    console.log(response);
+    
+    expect(response.body.response).toBeInstanceOf(Array);
+  })
+});
+
+describe("POST /products", () => {
+  test("Should respond with 200 status code", async () => {
+    const response = await request(app).post("/api/v1/products").send();
+    expect(response.statusCode).toBe(201);
+  });
+
+  
 });
